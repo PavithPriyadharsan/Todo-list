aa9ae19b7371cdffbd6d373a4ee4f3a3598d5c34
PavithPriyadharsan <146210938+PavithPriyadharsan@users.noreply.github.com>
2024-01-20 16:31:03 +0530

    To-do list with svelte-kit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Header from "./components/Header.svelte";
+	import List from "./components/List.svelte";
+</script>
+
+
+
+<style>
+	main{
+		background-color: var(--color-grey);
+    	font-family: "Roboto", sans-serif;
+    	margin: 0;
+    	min-height: 80vh;
+    	display: flex;
+    	justify-content: center;
+    	align-items: center;
+	}
+	.container {
+	    width: 500px;
+		border-radius: 10px;
+		background: grey;
+	
+	}
+
+	:global(.container *) {
+		font-family: sans-serif;
+	}
+</style>
+<main>
+	<div class="container">
+		<Header></Header>
+		<List></List>
+	</div>	
+</main>
+
